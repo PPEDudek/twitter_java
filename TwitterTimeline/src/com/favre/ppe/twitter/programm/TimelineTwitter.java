5f2e90a255d8d1776db9e585c195fea26b934547
DESKTOP-33TGNI0\favre <kevin.favre@ynov.com>
2016-03-10 17:06:03 +0100

    Authorization Required 401

--- TwitterTimeline/src/com/favre/ppe/twitter/programm/TimelineTwitter.java
+++ TwitterTimeline/src/com/favre/ppe/twitter/programm/TimelineTwitter.java
@@ -1,21 +1,13 @@
 package com.favre.ppe.twitter.programm;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-
-import com.favre.ppe.twitter.authentification.java.TwitterAuthHeader;
 import com.favre.ppe.twitter.gui.java.MainWindow;
 
 public class TimelineTwitter {
 
-	public static void main(String[] args) throws InvalidKeyException, NoSuchAlgorithmException, URISyntaxException, IOException {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MainWindow mw = new MainWindow();
 		
-		TwitterAuthHeader tah = new TwitterAuthHeader();
-		tah.getTwitterAuthHeader();
 	}
 
 }
